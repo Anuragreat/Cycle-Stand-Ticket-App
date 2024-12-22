b301459b19c672784c4b2448c50a8c6ecd4c0275
Anuragreat <ay3898097@gmail.com>
2024-12-22 19:06:55 +0530

    Initial commit of Cycle Stand Ticket App

---
+++ src/main/java/com/example/cycle_stand_ticket_app/CycleStandTicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cycle_stand_ticket_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleStandTicketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycleStandTicketAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cycle_stand_ticket_app/CycleStandTicketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cycle_stand_ticket_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleStandTicketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
